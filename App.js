065068754a4c747b45ab1aef28e353ec4ac75881
kenkono <twentynresult@yahoo.co.jp>
2019-08-31 16:12:40 +0800

    navigation

--- App.js
+++ App.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
-import WelcomeScreen from './src/screens/WelcomeScreen';
+// import WelcomeScreen from './src/screens/WelcomeScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
+const App = createStackNavigator({
+  Home: { screen: MemoListScreen },
+  MemoDetail: { screen: MemoDetailScreen },
+  MemoEdit: { screen: MemoEditScreen },
+  Login: { screen: LoginScreen },
+  Signup: { screen: SignupScreen },
 
-        <Appbar />
-
-        <WelcomeScreen />
-
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFDF6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
+}, {
+  defaultNavigationOptions: {
+    headerTitle: 'Memot',
+    headerStyle: {
+      backgroundColor: '#265366',
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    },
   },
 });
+
+export default createAppContainer(App);
