e6bb951989645aea5e7f1a90a6cc5bceb232ef00
kenkono <twentynresult@yahoo.co.jp>
2019-08-31 18:42:25 +0800

    login and siginup firebase

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   StyleSheet, View, Text, TextInput, TouchableHighlight,
 } from 'react-native';
+import firebase from 'firebase';
 
 class LoginScreen extends React.Component {
   constructor(props) {
@@ -13,9 +14,14 @@ class LoginScreen extends React.Component {
   }
 
   handleSubmit() {
-    // { this.props.navigation.navigate('Home') }
-
-    // Log in
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then((user) => {
+        console.log('success', user);
+        this.props.navigation.navigate('Home')
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
   }
 
   render() {
